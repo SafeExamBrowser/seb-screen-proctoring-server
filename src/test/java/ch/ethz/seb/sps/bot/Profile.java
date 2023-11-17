ef3320d4455b0cadc9d4dfe1117c896d3a4d483e
Nadim Ritter <nadim.ritter@let.ethz.ch>
2023-11-17 09:36:11 +0100

    revert changes in profile

--- src/test/java/ch/ethz/seb/sps/bot/Profile.java
+++ src/test/java/ch/ethz/seb/sps/bot/Profile.java
@@ -29,7 +29,7 @@ public final class Profile {
     String accessTokenEndpoint = "/oauth/token";
 
     @JsonProperty("groupId")
-    String groupId = "testgroup1000";
+    String groupId = "testgroup100";
     @JsonProperty("sessionId")
     String sessionId = null;
     @JsonProperty("clientId")
@@ -38,9 +38,9 @@ public final class Profile {
     String clientSecret = "test";
 
     @JsonProperty("runtime")
-    int runtime = 20 * 60000;
+    int runtime = 1 * 60000;
     @JsonProperty("numberOfConnections")
-    int numberOfConnections = 1;
+    int numberOfConnections = 10;
     @JsonProperty("spawnDelay")
     long spawnDelay = 1000;
     @JsonProperty("screenshotIntervall")
