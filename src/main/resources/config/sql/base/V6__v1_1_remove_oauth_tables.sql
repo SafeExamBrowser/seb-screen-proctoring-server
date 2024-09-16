6f2ae007360c377f09193603fc098c527838831e
anhefti <andreas.hefti@let.ethz.ch>
2024-09-16 09:16:20 +0200

    SEBSP-115 added migration to remove old oauth tables

---
+++ src/main/resources/config/sql/base/V6__v1_1_remove_oauth_tables.sql
@@ -0,0 +1,6 @@
+-- -----------------------------------------------------
+-- Drop Table `oauth_access_token` and `oauth_refresh_token` SEBSP-115
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `oauth_access_token`;
+DROP TABLE IF EXISTS `oauth_refresh_token`;
+
