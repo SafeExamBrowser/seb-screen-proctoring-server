e3b79fa0cfcbc84721b94f6bc03b35b8d0cc2b71
anhefti <andreas.hefti@let.ethz.ch>
2024-09-23 12:13:17 +0200

    SEBSP-163 no deletion of oauth tables

--- src/main/resources/config/sql/base/V6__v1_1_remove_oauth_tables.sql
+++
@@ -1,6 +0,0 @@
--- -----------------------------------------------------
--- Drop Table `oauth_access_token` and `oauth_refresh_token` SEBSP-115
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `oauth_access_token`;
-DROP TABLE IF EXISTS `oauth_refresh_token`;
-
