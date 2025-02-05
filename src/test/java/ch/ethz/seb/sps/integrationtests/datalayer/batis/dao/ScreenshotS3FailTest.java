1673c25dff694e0cbe1d4e7b20424682df2878cd
anhefti <andreas.hefti@let.ethz.ch>
2025-02-05 12:37:09 +0100

    SEBSP-200 add more tests

--- src/test/java/ch/ethz/seb/sps/integrationtests/datalayer/batis/dao/ScreenshotS3FailTest.java
+++ src/test/java/ch/ethz/seb/sps/integrationtests/datalayer/batis/dao/ScreenshotS3FailTest.java
@@ -10,7 +10,11 @@ package ch.ethz.seb.sps.integrationtests.datalayer.batis.dao;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+
+import ch.ethz.seb.sps.domain.model.service.Session;
 import ch.ethz.seb.sps.integrationtests.ServiceTest_FULL_RDBMS;
+import ch.ethz.seb.sps.server.ServiceConfig;
 import ch.ethz.seb.sps.server.datalayer.batis.mapper.ScreenshotDataRecordMapper;
 import ch.ethz.seb.sps.server.datalayer.batis.model.ScreenshotDataRecord;
 import ch.ethz.seb.sps.server.datalayer.dao.impl.S3DAO;
@@ -19,24 +23,32 @@ import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.transaction.PlatformTransactionManager;
+
 
 public class ScreenshotS3FailTest extends ServiceTest_FULL_RDBMS {
     
-//    @Autowired
-//    private ScreenshotStore_S3 screenshotStore_S3;
-    
-//    @Mock
-//    private S3DAO s3DAO;
+
+    @Mock
+    private S3DAO s3DAO;
 
     @Autowired
     private SqlSessionFactory sqlSessionFactory;
     @Autowired
     private ScreenshotDataRecordMapper screenshotDataRecordMapper;
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+    @Autowired
+    @Qualifier(value = ServiceConfig.SCREENSHOT_STORE_API_EXECUTOR) 
+    private TaskScheduler taskScheduler;
 
     @Test
-    public void testNoDuplicateScreenshotDataStoresOnS3Fail() {
+    public void testNoDuplicateScreenshotDataStores() {
 
         SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(this.sqlSessionFactory, ExecutorType.BATCH);
 
@@ -55,8 +67,38 @@ public class ScreenshotS3FailTest extends ServiceTest_FULL_RDBMS {
         }
         
         Long num = screenshotDataRecordMapper.countByExample().build().execute();
-        
-        assertTrue(1 == num);
 
+        assertEquals(1, (long) num);
+
+    }
+
+    @Test
+    public void testNoDuplicateScreenshotDataStoresOnS3Fail() {
+        
+        Mockito.when(s3DAO.uploadItem(Mockito.any(), Mockito.any(),Mockito.any()))
+                .thenThrow(new IllegalStateException("Test: No space left"));
+
+        ScreenshotStore_S3 screenshotStore_S3 = new ScreenshotStore_S3(
+                sqlSessionFactory,
+                transactionManager,
+                s3DAO,
+                taskScheduler,
+                1000, 
+                false
+        );
+
+        screenshotStore_S3.init();
+
+        screenshotStore_S3.storeScreenshot("session123", 100L, Session.ImageFormat.PNG, "metadata", new ByteArrayInputStream(new byte[] {1, 2}));
+        
+        // wait some time 
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            
+        }
+
+        Long num = screenshotDataRecordMapper.countByExample().build().execute();
+        assertEquals(1, (long) num);
     }
 }
