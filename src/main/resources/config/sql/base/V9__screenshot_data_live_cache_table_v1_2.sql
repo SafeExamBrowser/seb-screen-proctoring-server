1cf3cdf94667c8fa9b405ec34d80df192bc3031e
anhefti <andreas.hefti@let.ethz.ch>
2025-08-27 08:29:30 +0200

    SEBSP-210 ssd-id is nullable

--- src/main/resources/config/sql/base/V9__screenshot_data_live_cache_table_v1_2.sql
+++ src/main/resources/config/sql/base/V9__screenshot_data_live_cache_table_v1_2.sql
@@ -1,11 +1,9 @@
 -- -----------------------------------------------------
 -- Table `screenshot_data_live_cache` SEBSP-210
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `screenshot_data_live_cache` ;
-
 CREATE TABLE IF NOT EXISTS `screenshot_data_live_cache` (
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `session_uuid` VARCHAR(45) NOT NULL,
-  `id_latest_ssd` BIGINT UNSIGNED NOT NULL,
+  `id_latest_ssd` BIGINT UNSIGNED NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_session_uuid` (`session_uuid` ASC));
